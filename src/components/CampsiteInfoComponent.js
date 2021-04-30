4795ace8db2be1ef8a1a8d6998991e1f667f7a15
Jaelyn Denise <jaelyn.denise@gmail.com>
2021-04-29 20:13:49 -0700

    React Redux Forms Revisited

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -34,12 +34,12 @@ function RenderComments({comments, addComment, campsiteId}) {
                     return(
                         <div key={comment.id}>
                             <p>{comment.text}<br />
-                            --{comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                --{comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                             </p>
                         </div>
                     );
                 })} 
-                <CommentForm campsiteId-={campsiteId} addComment={addComment}/>
+                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
             </div>
         );
     }
